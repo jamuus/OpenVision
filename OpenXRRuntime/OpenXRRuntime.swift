d334f0fffc38ec8de159b5d832a92e3b5a9d1a3a
Jam <jamessewart@gmail.com>
2025-02-28 20:42:23 +0000

    Initial Commit

---
+++ OpenXRRuntime/OpenXRRuntime.swift
@@ -0,0 +1,9 @@
+//
+//  OpenXRRuntime.swift
+//  OpenXRRuntime
+//
+//  Created by James Sewart on 28/02/2025.
+//
+
+import Foundation
+
---
+++ OpenXRRuntimeTests/OpenXRRuntimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenXRRuntimeTests.swift
+//  OpenXRRuntimeTests
+//
+//  Created by James Sewart on 28/02/2025.
+//
+
+import Testing
+@testable import OpenXRRuntime
+
+struct OpenXRRuntimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
